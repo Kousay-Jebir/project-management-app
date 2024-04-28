1bfdb7eda066d55fa70dbaaf711752ba82744b0f
chedli01 <chedli.masmoudi01@gmail.com>
2024-04-28 18:19:51 +0200

    login

---
+++ src/Controller/MainpageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainpageController extends AbstractController
+{
+    #[Route('/mainpage', name: 'app_mainpage')]
+    public function index(): Response
+    {$user=$this->getUser();
+        
+        if($user){return $this->render('mainpage/index.html.twig', [
+            'controller_name' => 'MainpageController',
+        ]);}
+        else{return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+            
+
+        }
+        
+    }
+}
