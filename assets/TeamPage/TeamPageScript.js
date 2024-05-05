2b7d0e74ef7869c8b4c1f5be34dbce97dee1ffbf
YasmineZerai <zeraiyasmine7@gmail.com>
2024-05-05 11:22:53 +0100

    connected the TeamPaga to the MainPage and started to fill it with the appropriate data

--- assets/TeamPage/TeamPageScript.js
+++ assets/TeamPage/TeamPageScript.js
@@ -7,8 +7,22 @@ let content=document.getElementById("content")
 membersButton.addEventListener('click',showMembers)
 projectsButton.addEventListener('click',showProjects)
 overviewButton.addEventListener('click',showOverview)
-function showOverview(){
-   content.innerHTML=`<div></div>`
+async function showOverview(){
+   let id=document.getElementById("team_id").innerText;
+   content.innerHTML=`<div id="overview-content"class="overview-content hidden">
+   <div class="team-description">
+       <h1>Team Description:</h1>
+       <p style="font-size: 20px;" id="description"></p>
+   </div>
+   <div class="team-current-tasks">
+       <h1>Current Tasks:</h1>
+       <li class="task-component">
+           ourhfrzi
+       </li>
+   </div>
+ </div>`
+ await fetchData(id,"overview").then(data=>{document.getElementById("description").innerHTML=data.teamDescription})
+ document.getElementById("overview-content").classList.remove("hidden")
 }
 function showProjects(){
     content.innerHTML=`<div>Projects</div>`
@@ -16,3 +30,20 @@ function showProjects(){
  function showMembers(){
     content.innerHTML=`<div>Members</div>`
  }
+ async function fetchData(id,action) {
+
+   const url = `/TeamPage/${id}/${action}`;
+   return fetch(url, {
+       method: 'GET',
+       headers: {
+           'Content-Type': 'application/json'
+       }
+   })
+       .then(response => {
+           if (!response.ok) {
+               throw new Error('Failed to fetch tasks');
+           }
+           console.log(response)
+           return response.json();
+       });
+}
--- src/Controller/TeamPageController.php
+++ src/Controller/TeamPageController.php
@@ -8,12 +8,16 @@ use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Team;
 use App\Entity\User;
+use App\Repository\TeamRepository;
+use App\Repository\TaskRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class TeamPageController extends AbstractController
 {
     #[Route('TeamPage/{id}', name: 'app_team_page')]
     public function index(EntityManagerInterface $entityManager,$id): Response
     {
+        
         $user=$this->getUser();
         $team=$entityManager->getRepository(Team:: class)->findOneBy(['id'=>$id]);
         $leader_id=$team->getTeamLeader()->getId();
@@ -22,7 +26,16 @@ class TeamPageController extends AbstractController
         return $this->render('team_page/index.html.twig', [
             'controller_name' => 'TeamPageController',
             'team'=>$team,
-            'leader'=>$team_leader
+            'leader'=>$team_leader,
+            'user'=>$user,
+            'team_id'=>$id
         ]);
     }
+    #[Route('/TeamPage/{id}/overview', name: 'team_page_overview', methods: ['GET'])]
+    public function getOverviewData(TeamRepository $teamRepository, Request $request,$id): Response
+    {   
+        $team=$teamRepository->findOneBy(['id'=>$id]);
+        return $this->json(['teamDescription'=>$team->getTeamDescription()]);
+    }
+   
 }
